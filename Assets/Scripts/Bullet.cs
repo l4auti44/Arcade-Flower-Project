6e2a4510f8c245af5111a7580366955cfb5b1488
lautaro lamberti <lautarolamberti.44@gmail.com>
2023-01-07 15:02:59 -0300

    Refact Spawner code for multiple enemies

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,17 +7,21 @@ public class Bullet : MonoBehaviour
     public float speed = 3f;
     public float damage = 20f;
     
-    private GameObject player;
-
-    private bool look = false;
     private float targetTime = 0.2f;
     private float offset;
+    private int randomDirection;
+    private float xWalls, yWalls;
+    private float positionSpawnX, positionSpawnY;
 
     // Start is called before the first frame update
     void Start()
     {
+        xWalls = GameManager.leftRightWall - 0.1f;
+        yWalls = GameManager.topBottom - 0.1f;
 
+        spawnDirection();
         setRotation();
+
 
     }
 
@@ -25,19 +29,19 @@ public class Bullet : MonoBehaviour
     {
         var x = transform.position.x; 
         var y = transform.position.y;
-        var wallx = GameManager.leftRightWall - 0.1f;
-        var wally = GameManager.topBottom - 0.1f;
+        
+
         offset = Random.Range(-30f, 30f);
-        if (x == wallx)
+        if (x == xWalls)
         {
             transform.Rotate(new Vector3(0, 0, 180f + offset));
 
         }
-        else if (x == -wallx)
+        else if (x == -xWalls)
         {
             transform.Rotate(new Vector3(0, 0, 0f + offset));
         }
-        else if (y == wally) 
+        else if (y == yWalls) 
         {
             transform.Rotate(new Vector3(0, 0, 270f + offset));
         }
@@ -52,6 +56,8 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+
         targetTime -= Time.deltaTime;
 
 
@@ -65,14 +71,45 @@ public class Bullet : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void spawnDirection()
     {
+        randomDirection = Random.Range(0, 4);
 
+        positionSpawnX = Random.Range(-xWalls, xWalls);
+        positionSpawnY = Random.Range(-yWalls, yWalls);
+
+        //rigth
+        if (randomDirection == 0)
+        {
+            transform.position = new Vector3(xWalls, positionSpawnY, 0);
+        }
+        //bottom
+        else if (randomDirection == 1)
+        {
+            transform.position = new Vector3(positionSpawnX, -yWalls, 0);
+        }
+        //top
+        else if (randomDirection == 2)
+        {
+            transform.position = new Vector3(positionSpawnX, yWalls, 0);
+        }
+        //left
+        else
+        {
+            transform.position = new Vector3(positionSpawnX, yWalls, 0);
+        }
     }
+
+
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        collision.gameObject.GetComponent<Health>().decreaseHealth(damage);
-        GameObject.Destroy(this.gameObject);
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Health>().decreaseHealth(damage);
+            GameObject.Destroy(this.gameObject);
+        }
+
     }
 }
--- Assets/Scripts/spawner.cs
+++ Assets/Scripts/spawner.cs
@@ -5,15 +5,11 @@ public class spawner : MonoBehaviour
     public GameObject enemy;
 
     private float targetTime = 1f;
-    private int randomDirection;
-    private float positionSpawnX, positionSpawnY;
-    private float xWalls, yWalls;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        xWalls = GameManager.leftRightWall - 0.1f;
-        yWalls = GameManager.topBottom - 0.1f;
 
     }
 
@@ -23,37 +19,7 @@ public class spawner : MonoBehaviour
         
         if (targetTime <= 0.0f)
         {
-
-            randomDirection = Random.Range(0, 4);
-
-            positionSpawnX = Random.Range(-xWalls, xWalls);
-            positionSpawnY = Random.Range(-yWalls, yWalls);
-
-            Debug.Log(xWalls + " " + yWalls);
-
-            //rigth
-            if (randomDirection == 0)
-            {
-                GameObject.Instantiate(enemy, new Vector3(xWalls, positionSpawnY, 0), this.transform.rotation);
-            }
-            //bottom
-            else if (randomDirection == 1)
-            {
-                GameObject.Instantiate(enemy, new Vector3(positionSpawnX, -yWalls, 0), this.transform.rotation);
-            }
-            //top
-            else if (randomDirection == 2)
-            {
-
-                GameObject.Instantiate(enemy, new Vector3(positionSpawnX, yWalls, 0), this.transform.rotation);
-            }
-            //left
-            else
-            {
-
-                GameObject.Instantiate(enemy, new Vector3(-xWalls, positionSpawnY, 0), this.transform.rotation);
-            }
-            
+            GameObject.Instantiate(enemy, enemy.transform.position, this.transform.rotation);
             targetTime = 1f;
         }
 
