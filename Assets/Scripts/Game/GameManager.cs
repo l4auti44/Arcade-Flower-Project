248db8ed1eca5cc2003059f322f40a10a8eec40d
lautaro lamberti <lautarolamberti.44@gmail.com>
2023-01-22 17:39:57 -0300

    redone pellet UI

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -19,21 +19,10 @@ public class GameManager : MonoBehaviour
     public static float leftRightWall;
     public static float topBottom;
     public static int pellets = 0;
-    [SerializeField] private Slider pelletsSllider;
+    [SerializeField] private Image pelletsSllider;
 
     
 
-    private void Awake()
-    {
-        //if (Instance != null)
-        //    Destroy(gameObject);
-        //    Instance.Start();
-        //    return;
-        //}
-
-        //Instance = this;
-        //DontDestroyOnLoad(gameObject);
-    }
     // Start is called before the first frame update
     void Start()
     {
@@ -43,6 +32,7 @@ public class GameManager : MonoBehaviour
         points = GameObject.Find("points").GetComponent<TextMeshProUGUI>();
         numberPoints = 0;
         pellets = 0;
+        pelletsSllider.rectTransform.localScale = new Vector2(0f, 1f);
         points.text = "Points: " + numberPoints;
     }
 
@@ -57,8 +47,6 @@ public class GameManager : MonoBehaviour
             points.text = "Points: " + numberPoints;
             time += 1;
         }
-        
-        
 
 
 
@@ -66,23 +54,30 @@ public class GameManager : MonoBehaviour
 
     public void pickUpPellet()
     {
-        pellets++;
-        refreshPelletsText();
+        if (pellets < 7)
+        {
+            pellets++;
+            refreshPelletsText();
+        }
+        
     }
 
     private void refreshPelletsText()
     {
         Debug.Log(pellets);
-        Image fillpel = GameObject.Find("FillPellets").GetComponent<Image>();
+        
         if (pellets == 0)
         {
-            fillpel.enabled = false;
+            pelletsSllider.enabled = false;
         }
         else
         {
-            fillpel.enabled = true;
+            pelletsSllider.enabled = true;
         }
-        pelletsSllider.value = pellets;
+     
+        pelletsSllider.rectTransform.localScale = new Vector2(pelletsSllider.rectTransform.localScale.x + 1f, 1f);
+       
+        
     }
 
 }
